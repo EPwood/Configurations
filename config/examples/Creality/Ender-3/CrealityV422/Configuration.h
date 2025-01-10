d71b8193e0eb477c0e97fc7ee9785c8b9ce72d51
Eric <eric@prestwood.me>
2025-01-10 09:14:00 -0800

    Added more configs for ABL and Z Homing

--- config/examples/Creality/Ender-3/CrealityV422/Configuration.h
+++ config/examples/Creality/Ender-3/CrealityV422/Configuration.h
@@ -2113,7 +2113,7 @@
 //#define AUTO_BED_LEVELING_3POINT
 //#define AUTO_BED_LEVELING_LINEAR
 //#define AUTO_BED_LEVELING_BILINEAR
-//#define AUTO_BED_LEVELING_UBL
+#define AUTO_BED_LEVELING_UBL
 //#define MESH_BED_LEVELING
 
 /**
@@ -2348,7 +2348,7 @@
  * - Allows Z homing only when XY positions are known and trusted.
  * - If stepper drivers sleep, XY homing may be required again before Z homing.
  */
-//#define Z_SAFE_HOMING
+#define Z_SAFE_HOMING
 
 #if ENABLED(Z_SAFE_HOMING)
   #define Z_SAFE_HOMING_X_POINT X_CENTER  // (mm) X point for Z homing
